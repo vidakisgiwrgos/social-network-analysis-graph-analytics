3a96ad53c7ebfb44d2f13043fd483f5efecddf4c
Giorgos Vidakis <vidakisgiwrgos@gmail.com>
2026-03-03 14:27:50 +0200

    Update fb_pagerank2.py
    paths fix

--- facebook_analysis/fb_pagerank2.py
+++ facebook_analysis/fb_pagerank2.py
@@ -1,4 +1,8 @@
 import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_DIR = os.path.join(BASE_DIR, "data")
+
 import networkx as nx
 
 dataset_dir = r"C:\Users\User\Videos\sn_project\facebook"
