a5ec3029da53e921dba124f907c19c0fd637be55
Giorgos Vidakis <vidakisgiwrgos@gmail.com>
2026-03-03 14:25:49 +0200

    Update citations_communities.py
    paths fix

--- citation_analysis/citations_communities.py
+++ citation_analysis/citations_communities.py
@@ -1,4 +1,8 @@
 import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_DIR = os.path.join(BASE_DIR, "data")
+
 import random
 import pandas as pd
 import networkx as nx
